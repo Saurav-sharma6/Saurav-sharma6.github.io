a4457bbc59dd9ae27233c5a1268b81296d293308
Saurav Sharma <sauravsharma691999@gmail.com>
2025-05-28 15:42:56 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// toggle code
+const toggle = document.querySelector(".toggle-switch input");
+console.log(toggle);
+let flag = true;
+
+toggle.addEventListener("click", () => {
+  if (flag) {
+    document.body.style.backgroundColor = "rgb(13, 13, 13)";
+    document.body.style.color = "#FFFFFF";
+    flag = false;
+  } else {
+    document.body.style.backgroundColor = "#fffefefb";
+    document.body.style.color = "#000000";
+    flag = true;
+  }
+  console.log(flag);
+});
+
+// Sparkling code
+
+const container = document.querySelector(".container");
+
+function createSparkle() {
+  const sparkle = document.createElement("div");
+  sparkle.classList.add("sparkle");
+
+  // Random horizontal position
+  sparkle.style.left = `${Math.random() * 100}vw`;
+  sparkle.style.top = `${Math.random() * 100}vh`;
+
+  // Optional: Random animation duration
+  sparkle.style.animationDuration = `${Math.random() * 2 + 1.5}s`;
+  sparkle.style.background = flag
+  ? "radial-gradient(circle, rgb(14, 14, 14), transparent)"
+  : "radial-gradient(circle, white, transparent)";
+
+  container.appendChild(sparkle);
+
+  // Remove sparkle after animation
+  setTimeout(() => {
+    sparkle.remove();
+  }, 5000);
+}
+
+// Create sparkles at an interval
+setInterval(createSparkle,70); // every 150ms
